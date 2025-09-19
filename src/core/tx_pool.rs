d148aec1fa9c8db079ea9514d9f4a275a6b5b91d
blockora <64456089+blockora@users.noreply.github.com>
2025-09-19 23:49:17 +0530

    Update tx_pool.rs

--- src/core/tx_pool.rs
+++ src/core/tx_pool.rs
@@ -1,1 +1,27 @@
+use crate::wallet::wallet::Transaction;
+use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub struct TxPool {
+    pub pool: Vec<Transaction>,
+}
+
+impl TxPool {
+    pub fn new() -> Self {
+        Self { pool: Vec::new() }
+    }
+
+    pub fn add_tx(&mut self, tx: Transaction) {
+        self.pool.push(tx);
+    }
+
+    pub fn take_all(&mut self) -> Vec<Transaction> {
+        let txs = self.pool.clone();
+        self.pool.clear();
+        txs
+    }
+
+    pub fn len(&self) -> usize {
+        self.pool.len()
+    }
+}
