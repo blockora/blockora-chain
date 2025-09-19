39deae1feea43040172d25a85cbf37be517944e0
blockora <64456089+blockora@users.noreply.github.com>
2025-09-19 22:36:52 +0530

    Update main.rs

--- cli/main.rs
+++ cli/main.rs
@@ -1,8 +1,14 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-mod core;
+use blockora_chain::core::block::Block;
+use blockora_chain::network::p2p;
 
 fn main() {
-    let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
-    let genesis_block = core::block::Block::new(0, timestamp, String::from("0"), String::from("Genesis Block"));
-    println!("Genesis Block Created: {:?}", genesis_block);
+    println!("🚀 Blockora CLI Started!");
+
+    // Start Genesis Block
+    let genesis = Block::genesis();
+    println!("✅ Genesis Block Created: {:?}", genesis);
+
+    // Start P2P Network
+    println!("🌐 Starting P2P Node...");
+    p2p::start_server("127.0.0.1:6000");
 }
