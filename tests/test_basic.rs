16cb415acc1cc157e6982b4d5c23973c86b975f2
blockora <64456089+blockora@users.noreply.github.com>
2025-09-20 03:59:21 +0530

    Update test_basic.rs

--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -1,1 +1,12 @@
+use blockora_chain::core::chain::Blockchain;
+use blockora_chain::core::block::Block;
 
+#[test]
+fn test_genesis_and_add_block() {
+    let mut bc = Blockchain::new();
+    assert_eq!(bc.chain.len(), 1);
+    assert_eq!(bc.chain[0].index, 0);
+    let _ = bc.add_block("test-data".to_string());
+    assert_eq!(bc.chain.len(), 2);
+    assert!(bc.is_valid());
+}
