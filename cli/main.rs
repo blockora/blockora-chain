b7a48dfa09b97aaf081ee5ad1e8f63b211f4950d
blockora <64456089+blockora@users.noreply.github.com>
2025-09-19 22:19:09 +0530

    Update main.rs

--- cli/main.rs
+++ cli/main.rs
@@ -1,1 +1,8 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+mod core;
 
+fn main() {
+    let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+    let genesis_block = core::block::Block::new(0, timestamp, String::from("0"), String::from("Genesis Block"));
+    println!("Genesis Block Created: {:?}", genesis_block);
+}
